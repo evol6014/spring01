a7ca13e75716ea650ac786d142727bd4e9302504
java <java@bigdata>
2017-09-06 17:51:53 +0900

    aaaad

--- src/main/java/com/example/JavaLoggingExample.java
+++ src/main/java/com/example/JavaLoggingExample.java
@@ -10,6 +10,6 @@ public class JavaLoggingExample {
 	public static void main(String[] args) {
 		System.out.println("Hello Sysout logging...");
 	
-		log.info("Hello Java 내장 로그로 출력...");
+		log.info("Hello Java 내장 로그로 출력..");
 	}
 }
